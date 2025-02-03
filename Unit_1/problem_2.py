1df5efef57146da0184a6f459529b7ec39a0c0cf
Cynthia-Wairimu <wairimucee@gmail.com>
2025-02-03 14:12:36 +0300

    define a variable in problem_2.py

--- Unit_1/problem_2.py
+++ Unit_1/problem_2.py
@@ -6,15 +6,22 @@ then your program should print
 
 Number of times bob occurs is: 2
 """
+#for educational purposes, I will define the variable
+s = 'azcbobobegghakl'
 
 #initialize count of 'bob' substrings
 bob_count = 0
 
 #iterate through each character in the string s
-for i in range(len(s) - 2):  #stop 2 characters before the end of the string to avoid out of bounds error.
+for i in range(len(s) - 2):  #stop 2 characters before the end of the string to avoid out of bounds error. 
+    #When searching for a substring of length N, your loop should stop N-1 characters before the end of the string.
+    #for example, when searching for a substring of length 3, N=3, so the loop should stop at 3-1=2
+    
     #if the substring of the next 3 characters is 'bob', increment the bob count
-    if s[i:i+3] == 'bob':
+    #When searching for a substring of length, use index-based loops instead of character-based loops.add
+    if s[i:i+3] == 'bob': # Extracts 3 characters (perfect for "bob") starting from index i excluding the character at index i+3
         bob_count += 1
         
 #print the bob count
 print("Number of times bob occurs is: " + str(bob_count))
+
